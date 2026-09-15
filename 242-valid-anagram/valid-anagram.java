990c440196892821b7bfe3d5092d3edcca1d0fd1
Nandhakumar <168160991+Nandhakumar2006@users.noreply.github.com>
2026-09-15 21:44:57 +0530

    Time: 32 ms (5.14%) | Memory: 47.2 MB (9.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,20 +1,25 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        StringBuilder sb = new StringBuilder(s);
-        StringBuilder tb = new StringBuilder(t);
+        Map<Character,Integer> map1 = new HashMap<>();
+        Map<Character,Integer> map2 = new HashMap<>();
 
-        for(int i=0;i<sb.length();i++){
-            for(int j=0;j<tb.length();j++){
-                if(sb.charAt(i)==tb.charAt(j)){
-                    sb.deleteCharAt(i);
-                    tb.deleteCharAt(j);  
-                    i--;
-                    j--;
-                    break;
-                }
+        if(s.length()!=t.length()) return false;
+
+        for(int i=0;i<s.length();i++){
+            if(map1.containsKey(s.charAt(i))){
+                map1.put(s.charAt(i),map1.get(s.charAt(i))+1);
+            }
+            else{
+                map1.put(s.charAt(i),1);
+            }
+            if(map2.containsKey(t.charAt(i))){
+                map2.put(t.charAt(i),map2.get(t.charAt(i))+1);
+            }
+            else{
+                map2.put(t.charAt(i),1);
             }
         }
 
-         return sb.length()==0 && tb.length()==0;
+        return map1.equals(map2);
     }
 }
