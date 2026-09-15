6461cd3411e95b84614835c38268f20c1f4087af
Nandhakumar <168160991+Nandhakumar2006@users.noreply.github.com>
2026-09-15 21:57:11 +0530

    Time: 5 ms (62.97%) | Memory: 44.4 MB (66.18%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,25 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Map<Character,Integer> map1 = new HashMap<>();
-        Map<Character,Integer> map2 = new HashMap<>();
-
+        
         if(s.length()!=t.length()) return false;
 
+        int[] frequency = new int[26];
+
         for(int i=0;i<s.length();i++){
-            if(map1.containsKey(s.charAt(i))){
-                map1.put(s.charAt(i),map1.get(s.charAt(i))+1);
-            }
-            else{
-                map1.put(s.charAt(i),1);
-            }
-            if(map2.containsKey(t.charAt(i))){
-                map2.put(t.charAt(i),map2.get(t.charAt(i))+1);
-            }
-            else{
-                map2.put(t.charAt(i),1);
-            }
+            frequency[s.charAt(i)-'a']++;
+            frequency[t.charAt(i)-'a']--;
         }
 
-        return map1.equals(map2);
+        for(int x:frequency){
+            if(x!=0) return false;
+        }
+
+        return true;
     }
 }
