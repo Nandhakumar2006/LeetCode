63f49892c0835c51241cf9746b44f1d5091ded09
Nandhakumar <168160991+Nandhakumar2006@users.noreply.github.com>
2026-09-20 22:43:07 +0530

    Time: 7 ms (66.50%) | Memory: 43.5 MB (98.26%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,38 +1,34 @@
 class Solution {
-    public boolean checkInclusion(String s1, String s2) {
-        HashMap<Character,Integer> s1map = new HashMap<>();
-        HashMap<Character,Integer> s2map = new HashMap<>();
 
-        for(char s:s1.toCharArray()){
-            if(s1map.containsKey(s)) s1map.put(s,s1map.get(s)+1);
-            else s1map.put(s,1);
+    boolean matches(int[] arr1,int[] arr2){
+        for(int i=0;i<26;i++){
+            if(arr1[i]!=arr2[i]) return false;
+        }
+
+        return true;
+    }
+    
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()) return false;
+
+        int[] s1fre = new int[26];
+        int[] s2fre = new int[26];
+
+        for(char s1char:s1.toCharArray()){
+            s1fre[s1char-'a']++;
         }
 
         for(int i=0;i<s2.length();i++){
-            if(s2map.containsKey(s2.charAt(i))){
-                s2map.put(s2.charAt(i),s2map.get(s2.charAt(i))+1);
-                // System.out.println(s2.charAt(i));
-            }
-            else{
-                s2map.put(s2.charAt(i),1);
-            }
+            s2fre[s2.charAt(i)-'a']++;
+
             if(i>=s1.length()){
-                if(s2map.get(s2.charAt(i-s1.length()))>1){
-                    s2map.put(s2.charAt(i-s1.length()),s2map.get(s2.charAt(i-s1.length()))-1);
-                }
-                else{
-                s2map.remove(s2.charAt(i-s1.length()));
-                }
+                s2fre[s2.charAt(i-s1.length())-'a']--;
             }
 
-            // System.out.println("s1map : "+s1map);
-            // System.out.println("s2map : "+s2map);
-            if(s1map.equals(s2map) && i>=s1.length()-1) return true;
-
+            if(i>=s1.length()-1 && matches(s1fre,s2fre) ) return true;
+            
         }
 
         return false;
-
-        
     }
 }
